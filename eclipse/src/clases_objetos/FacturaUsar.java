683e93e02f99ef381a72743edabf7047af90649c
cetec-bchavez <98634131+cetec-bchavez@users.noreply.github.com>
2022-12-17 13:20:16 -0500

    FActura

--- eclipse/src/clases_objetos/FacturaUsar.java
+++ eclipse/src/clases_objetos/FacturaUsar.java
@@ -1,5 +1,7 @@
 package clases_objetos;
 
+import java.util.ArrayList;
+
 public class FacturaUsar {
 
 	public static void main(String[] args) {
@@ -17,6 +19,45 @@ public class FacturaUsar {
 		
 		System.out.println("Nombre= " + factura1.nombre + ", Cantidad= "+factura1.cantidad + ", Precio= "+factura1.precio + ", Total="+factura1.total);
 		
+		
+		
+		FacturaClase factura2 = new FacturaClase();
+
+		//Ingredientes
+		factura2.nombre="Teclado";
+		factura2.cantidad=50;
+		factura2.precio=3.5;
+		
+		//Procesar
+		factura2.calcularTotal();
+		
+		System.out.println("Nombre= " + factura2.nombre + ", Cantidad= "+factura2.cantidad + ", Precio= "+factura2.precio + ", Total="+factura2.total);
+		
+		
+		FacturaClase factura3 = new FacturaClase();
+
+		//Ingredientes
+		factura3.nombre="Monitor";
+		factura3.cantidad=70;
+		factura3.precio=4.5;
+		
+		//Procesar
+		factura3.calcularTotal();
+		
+		System.out.println("Nombre= " + factura3.nombre + ", Cantidad= "+factura3.cantidad + ", Precio= "+factura3.precio + ", Total="+factura3.total);
+		
+		ArrayList<FacturaClase> facturas = new ArrayList<FacturaClase>();
+		//new hoja Excel
+		
+		facturas.add(factura1);
+		facturas.add(factura2);
+		facturas.add(factura3);
+		
+		System.out.println("---------- LISTA COMPLETA DE DATOS ----------");
+		
+		for(FacturaClase fac1 : facturas) {
+			System.out.println("Nombre= " + fac1.nombre + ", Cantidad= "+fac1.cantidad + ", Precio= "+fac1.precio + ", Total="+fac1.total);			
+		}
 	}
 
 }
